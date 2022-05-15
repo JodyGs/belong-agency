cb7a143d5a9500023dd68683210e7d04b9118f78
JodyGs <demajn@tuta.io>
2022-05-15 13:08:10 +0200

    start project

---
+++ components/overlay.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function overlay() {
+  return (
+    <div className='h-screen w-full bg-belorange'>
+      <div>
+        <button>Français</button>
+        <button>English</button>
+      </div>
+    </div>
+  )
+}
+
+export default overlay
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        belorange: '#FD4000',
+        belvert: '#23EB83',
+        belrose: '#F19CAE',
+        belviolet: '#4E4898',
+      }
+    },
+  },
+  plugins: [],
+}
