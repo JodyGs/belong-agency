7502a9e4474431a1eb1109dc6727a6f945518ff3
JodyGs <demajn@tuta.io>
2022-06-02 12:27:55 +0200

    sanity.js config

--- pages/work.js
+++ pages/work.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import Header from '../components/Header';
+import { sanityClient, urlFor } from '../sanity';
 
-function Work() {
+function Work({works}) {
+
+	console.log(works);
+	console.log(works);
+
 	return (
 		<div>
 			<Header />
 			<div>
 				<h1 className='animate-changeColor'>work</h1>
 			</div>
+			{/* <img src={urlFor(works[0].thumbnail).url()} alt="" /> */}
 		</div>
 	);
 }
 
 export default Work;
+
+export const getServerSideProps = async () => {
+  const query = `*[_type == "work"]{
+		_id,
+		title,
+		thumbnail,
+		tags,
+	} | order(_createdAt desc)`;
+
+  const works = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      works,
+    },
+  };
+};
+
+
+
+
+
