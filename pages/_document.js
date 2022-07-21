1d0d4c8a5d41bef11df63ca813719d77f5e270e1
JodyGs <demajn@tuta.io>
2022-07-21 11:05:35 +0200

    v2.9

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,10 @@ export default function Document() {
       <Head>
         <link rel="stylesheet" href="/fonts/fonts.css" />
         <link rel="shortcut icon" href="/favicon.ico" />
+        <title>A boutique agency with a focus on sustainable beauty</title>
+        <meta property="og:title" content="A boutique agency with a focus on sustainable beauty" key="title" />
+        <meta name="description" content="Agence de relations publiques qui élaborent des stratégies de communication sur mesure qui renforcent la notoriété de marque et créent la demande." />
+
       </Head>
       <body>
         <Main />
